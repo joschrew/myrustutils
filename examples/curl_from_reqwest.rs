a46b3d78fc205a97ad3b9014215cec886d55ec05
joschrew <jonas.schrewe@gwdg.de>
2023-08-31 14:27:55 +0200

    Restructure example for print_curl

--- examples/curl_from_reqwest.rs
+++ examples/curl_from_reqwest.rs
@@ -5,9 +5,14 @@ use reqwest::header::ACCEPT;
 use reqwest::Url;
 
 fn main() {
-    // GET command with authentication
-    let password = "not-the-real-password";
     let client = Client::new();
+    simple_get_with_auth(&client);
+    get_with_download(&client);
+    post_multipart(&client);
+    post_json_body(&client);
+}
+
+fn simple_get_with_auth(client: &Client) {
     let url = Url::parse("http://example.com/api/")
         .unwrap()
         .join("admin/import-status")
@@ -16,11 +21,27 @@ fn main() {
         .get(url)
         .header(ACCEPT, "application/json")
         .query(&[("username", "admin"), ("page", "0"), ("limit", "2")])
-        .basic_auth("admin", Some(password));
+        .basic_auth("admin", Some("secret"));
     print_curl(&request).unwrap();
+}
 
-    // POST multipart
+fn get_with_download(client: &Client) {
+    let url = Url::parse("http://example.com/api/")
+        .unwrap()
+        .join("workspace/test1")
+        .unwrap();
+    let request = client
+        .get(url)
+        .header(ACCEPT, "application/vnd.ocrd+zip");
+    // TODO: the output flag ("--output foo.zip") is not part of the request. Enable a user to
+    // optionally add to the generated curl-command. Create a new function print_curl3 or
+    // print_curl_custom which takes a string which is automatically appended to the command
+    print_curl(&request).unwrap();
+}
+
+fn post_multipart(client: &Client) {
     let workspace_path = "foo.zip";
+    let password = "secret";
     let url = Url::parse("http://example.com/")
         .unwrap()
         .join("workspace/test1")
@@ -38,8 +59,9 @@ fn main() {
         .multipart(form)
         .basic_auth("ocrd", Some(password));
     print_curl_multipart(&request, &[("workspace", "@foo.zip")], Some(password));
+}
 
-    // POST json body
+fn post_json_body(client: &Client) {
     let url = Url::parse("http://example.com/")
         .unwrap()
         .join("workflow")
@@ -49,6 +71,6 @@ fn main() {
     let request = client
         .post(url)
         .body(body)
-        .basic_auth("ocrd", Some(password));
+        .basic_auth("ocrd", Some("secret"));
     print_curl(&request).unwrap();
 }
